7e4bbd3957083fc9304e83a61a809123e3638a9e
Kadaine <kadaine.williams@mymona.uwi.edu>
2018-03-22 03:53:07 +0000

    fourth commit

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ UPLOAD_FOLDER = "./app/static/uploads"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:PROJECT1@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://iuqvewujlrzqrw:292b72464af40add8d49404cc4fc56f3941e370f8245d16e38cc55d4531ec408@ec2-54-243-210-70.compute-1.amazonaws.com:5432/de69424rhl6am2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 db = SQLAlchemy(app)
 
